909ea981d326e84c01deee3214cf3e744e482569
robertm <robert.mcgowan@spire.com>
2022-08-18 10:54:51 +0100

    Small modification to the buils-docker.sh script

--- build-docker.sh
+++ build-docker.sh
@@ -1,19 +1,18 @@
 #!/usr/bin/env bash
-set -euo pipefail
 
 # Install docker if not installed already
 DOCKER_INSTALL_STATUS=`dpkg -l | grep docker.io | grep -o ^..`
-if [ "x$DOCKER_INSTALL_STATUS" == "xii" ]
+if [ "x$DOCKER_INSTALL_STATUS" != "xii" ]
 then
-    echo "Docker already installed"
+    # build the docker image
+    docker build -t yocto-lemdig .
 else
     # install docker
     sudo apt-get install -y docker.io
     
     # Add user to the docker group
     sudo usermod -a -G docker ${USER}
+    
+    echo "Please reboot the machine to allow the user to use docker then re-run this script"
 fi
 
-
-# build the docker image
-docker build -t yocto-lemdig .
